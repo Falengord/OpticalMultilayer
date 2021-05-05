3df2bb5d539eed415ef6cddc0cb29ec63d48af31
AnnaLuise97 <82158024+AnnaLuise97@users.noreply.github.com>
2021-05-05 10:43:48 +0200

    simulazione GaN con slider

--- simGaN.py
+++ simGaN.py
@@ -1,12 +1,13 @@
-#Paolo Wetzl 2020
-
 #IMPORT LIBRARIES
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from scipy.optimize import curve_fit
 from scipy.stats import chisquare
-    
-from fun import material_nk_fnLN, material_nk_fnLNt, material_nk_fnGaN, material_nk_fnAl2O3, make_k, make_T_int, make_T_lay, absorption, T_inc
+from matplotlib.widgets import Slider, Button
+
+from fun import *
+%matplotlib qt
 
 π = np.pi
 
@@ -18,89 +19,167 @@ layer characteristics
 4: air 
 ''' 
 
-#roughness list in micron
-z_1 = 0.000#5
-z_2 = 0.015
+#roughness between Al2O3 and air in micron
 z_3 = 0
 
-
-# list of layer thicknesses in micron
-d_GaN = 5.57
+#layer thicknesses in micron
 d_Al2O3 = 500
 
-
 #urbach energies, urbach zero energy and zero absorption coefficient
-Eu_GaN = 0.019 #in eV
-E0_GaN = 3.56  #in eV from "Urbach–Martienssen tails in a wurtzite GaN epilayer", Chichibu 1997
 alpha0_GaN = 5e3
-
-
-#wavelenghts for the simulation in micron
-l_sx = 0.350
-l_dx = 2.5
-n_points = 20000
-lambda_list = np.linspace(l_sx, l_dx, n_points)
-
-
-#initialization of various things matrices
-T = []
-Tinc = []
-a_GaN = []
 
 #angle of incidence on the first layer
 th = 0 * π/180
 
+#load the measurement datafile to compare it with the simulation
+x, y = np.loadtxt('Dati/n-GaN hi res detail.txt', delimiter='\t', unpack=True)
+
+xdata = x*1e-3  #in micron
+ydata = y*1e-2
 
 #execute the various operations for each wavelength
-for λ in lambda_list:
-
-    #compute the refractive indeces of the various materials
-    nreGaN = material_nk_fnGaN(λ,'o')
-    nimGaN = make_k(λ, alpha0_GaN, E0_GaN, Eu_GaN)
-    a_GaN.append(absorption(λ, alpha0_GaN, E0_GaN, Eu_GaN))
+def func(λ,d_GaN, E0_GaN, Eu_GaN, z_1, z_2, a, b1, c1):
     
-    n_GaN = nreGaN + 1j * nimGaN
+    A = a                     #3.6
+    B = [b1,4.1]              #1.75, 4.1
+    C = [c1,17.86**2]         #0.256**2, 17.86**2    
+    
+    #compute the refractive indeces of the various materials
+    nreGaN = n_sellmeier(λ,A,B,C)
+    nimGaN = make_k(λ, alpha0_GaN, E0_GaN, Eu_GaN) 
+     
+    n_GaN   = nreGaN + 1j * nimGaN
     n_Al2O3 = material_nk_fnAl2O3(λ, 'o')
-
+        
     #compute the transfer matrices for the various interfaces and layers, one by one
-    T12 = make_T_int(λ, 1, n_GaN, th, z_1)
+    T12 = make_T_int(λ, 1., n_GaN, th, z_1)
     T2  = make_T_lay(λ, n_GaN, d_GaN, th)
     T23 = make_T_int(λ, n_GaN, n_Al2O3, th, z_2)
     T3  = make_T_lay(λ, n_Al2O3, d_Al2O3, th)
-    T31 = make_T_int(λ, n_Al2O3, 1, th, z_3)
+    T31 = make_T_int(λ, n_Al2O3, 1., th, z_3)
+        
+    temp = np.einsum('iak,ajk->ijk',T12,T2)
+    temp = np.einsum('iak,ajk->ijk',temp,T23)
+    temp = np.einsum('iak,ajk->ijk',temp,T3)
 
-    #use eq 16 (Katsidis) for the computation of the transmission coefficient of the multilayer from the total transfer matrix for 
-    #the stack of layers
-    Tinc.append(T_inc(λ, np.dot(np.dot(T12,T2),T23), T31, n_Al2O3, d_Al2O3, th))
+    return T_inc(xdata, temp, T31, n_Al2O3, d_Al2O3, th) 
+
+fit = func(xdata,5.56,3.56,0.019,0.0,0.015,3.6,1.75,0.256**2)
+
+fig, ax = plt.subplots()
+plt.plot(xdata, ydata, 'k-', alpha=0.7, linewidth=0.8, label='Experimental data')
+line, = plt.plot(xdata, fit, 'r-', linewidth=0.8, label='Simulation')
+plt.legend()
+ax.set_xlabel('Wavelength (μm)')
+ax.set_ylabel('Fraction of power transmitted')
+
+axcolor = 'lightgoldenrodyellow'
+ax.margins(x=0)
+
+# adjust the main plot to make room for the sliders
+plt.subplots_adjust(top=0.975)
+plt.subplots_adjust(bottom=0.46)
+
+# Make a horizontal slider to control the thickness of GaN.
+axd = plt.axes([0.2, 0.1, 0.7, 0.015], facecolor=axcolor)
+d_slider = Slider(
+    ax=axd,
+    label='d_GaN [μm]',
+    valmin=4,
+    valmax=10,
+    valinit=5.57,
+    color="orange",
+)
+
+# Make a horizontal slider to control E0_GaN
+axE0 = plt.axes([0.2, 0.135, 0.7, 0.015], facecolor=axcolor)
+E0_slider = Slider(
+    ax=axE0,
+    label='E0_GaN [eV]',
+    valmin=3.51,
+    valmax=3.58,
+    valinit=3.56,
+    color="green",
+)
+
+# Make a horizontal slider to control Eu_GaN
+axEu = plt.axes([0.2, 0.17, 0.7, 0.015], facecolor=axcolor)
+Eu_slider = Slider(
+    ax=axEu,
+    label='Eu_GaN [eV]',
+    valmin=0.012,
+    valmax=0.022,
+    valinit=0.019,
+    color="green",
+)
+
+# Make a horizontal slider to control z_1
+axz1 = plt.axes([0.2, 0.205, 0.7, 0.015], facecolor=axcolor)
+z1_slider = Slider(
+    ax=axz1,
+    label='z_1 [μm]',
+    valmin=0,
+    valmax=0.02,
+    valinit=0,
+)
+
+# Make a horizontal slider to control z_2
+axz2 = plt.axes([0.2, 0.24, 0.7, 0.015], facecolor=axcolor)
+z2_slider = Slider(
+    ax=axz2,
+    label='z_2 [μm]',
+    valmin=0.012,
+    valmax=0.024,
+    valinit=0.015,
+)
+
+# Make a horizontal slider to control a
+axa = plt.axes([0.2, 0.275, 0.7, 0.015], facecolor=axcolor)
+a_slider = Slider(
+    ax=axa,
+    label='a',
+    valmin=3,
+    valmax=4.2,
+    valinit=3.6,
+    color='yellow',
+)
+
+# Make a horizontal slider to control b1
+axb1 = plt.axes([0.2, 0.31, 0.7, 0.015], facecolor=axcolor)
+b1_slider = Slider(
+    ax=axb1,
+    label='b1',
+    valmin=1,
+    valmax=2.2,
+    valinit=1.75,
+    color='yellow',
+)
+
+# Make a horizontal slider to control c1
+axc1 = plt.axes([0.2, 0.345, 0.7, 0.015], facecolor=axcolor)
+c1_slider = Slider(
+    ax=axc1,
+    label='c1',
+    valmin=0.2**2,
+    valmax=0.3**2,
+    valinit=0.256**2,
+    color='yellow',
+)
+
+# The function to be called anytime a slider's value changes
+def update(val):
+    line.set_ydata(func(xdata, d_slider.val, E0_slider.val, Eu_slider.val, z1_slider.val, z2_slider.val, a_slider.val, b1_slider.val, c1_slider.val))
+    fig.canvas.draw_idle()
 
 
-#load the measurement datafile to compare it with the simulation
-x, y = np.loadtxt('Dati/n-GaN hi res detail.txt', delimiter='\t', unpack=True)
-x2, y2 = np.loadtxt('Dati/n-GaN.txt', delimiter='\t', unpack=True)
-
-d = np.array([lambda_list,Tinc]).T
-np.savetxt('simGaN.txt', d, fmt='%10.5f', newline='\n')
-
-e = np.array([0.4135667662 * 2.99 / (x*1e-3), np.log(-np.log(y*1e-2))]).T
-np.savetxt('simGaN_u.txt', e, fmt='%10.5f', newline='\n')
-
-
-#plot the simulation and the measured data in the same graph
-plt.figure(1)
-plt.plot(0.4135667662 * 2.99 / lambda_list, Tinc, 'g-', alpha=0.7, linewidth=0.8)
-plt.plot(0.4135667662 * 2.99 / (x2*1e-3), y2*1e-2, 'b-', alpha=0.5, linewidth=0.8)
-plt.xlabel('Energy (eV)')
-plt.ylabel('Fraction of power transmitted')
-
-plt.title('Transmission at normal incidence')
-plt.savefig('Transmission_GaN.svg')
-
-plt.figure(2)
-plt.xlim(3, 3.4)
-plt.ylim(-2, 0)
-plt.plot(0.4135667662 * 2.99 / lambda_list , np.log(-np.log(Tinc)), 'g-', alpha=0.7, linewidth=0.8)
-plt.plot(0.4135667662 * 2.99 / (x2*1e-3) , np.log(-np.log(y2*1e-2)), 'b-', alpha=0.7, linewidth=0.8)
-plt.xlabel('Energy (eV)')
-plt.ylabel('ln(-ln(T))')
+# register the update function with each slider
+d_slider.on_changed(update)
+E0_slider.on_changed(update)
+Eu_slider.on_changed(update)
+z1_slider.on_changed(update)
+z2_slider.on_changed(update)
+a_slider.on_changed(update)
+b1_slider.on_changed(update)
+c1_slider.on_changed(update)
 
 plt.show()
